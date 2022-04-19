f988241ec67aed2ca86d62ff6a3faae9b8ce6e4f
Elijan Mastnak <ejmastnak@gmail.com>
2022-04-19 18:55:52 -0400

    doc: improve documentation of constants file

--- src/constants.py
+++ src/constants.py
@@ -1,21 +1,27 @@
 """
-Sample rate and filter specs related to the Hilbert transformer project.
+Sample rate, filter specs, and some plotting colors related to the Hilbert
+transformer project.
 """
 
 F_S = 44100         # sample rate [Hz]
 NYQUIST = F_S/2     # Nyquist rate [Hz]
 PI = 3.141592653589793
-M = 529  # number of filter coefficients
 
+# Number of coefficients used in the filter's impulse response. In principle
+# ~329 should be enough to meet the filter specifications, but you might want
+# to bump this up by a hundred or so for a narrower transition band.
+M = 329  
+
+# Stopband and passband frequencies
 F_STOP_L = 500      # [Hz]
 F_PASS_L = 750     # [Hz]
 F_PASS_R = 2200     # [Hz]
 F_STOP_R = 2500     # [Hz]
 
-# F0_HILBERT = 3500   # [Hz]
+# Frequency up to which Hilbert transformer will act
 F0_HILBERT = NYQUIST
 
-# normalized frequencies
+# Normalized versions of stopband and passband frequencies
 W_STOP_L = 2*PI*F_STOP_L/F_S
 W_PASS_L = 2*PI*F_PASS_L/F_S
 W_PASS_R = 2*PI*F_PASS_R/F_S
